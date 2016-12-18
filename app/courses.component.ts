4d293f019f8af6d384c15b03a35064911c85912a
mtraina <matteo.traina.mail@gmail.com>
2016-12-18 17:58:36 +0100

    courses list using nucor

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -5,8 +5,14 @@ import {Component} from 'angular2/core'
     template: `
         <h2>Courses</h2>
         {{ title }}
+        <ul>
+            <li *ngFor="#course of courses">
+            {{ course}}
+            </li>
+        </ul>
         `
 })
 export class CoursesComponent {
     title = "The title of courses page";
+    courses = ["Course1", "Course2", "Course3"]
 }
